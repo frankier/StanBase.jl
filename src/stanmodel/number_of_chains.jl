8f546bb29c0a3404bfbce0e9bfb6e8f595e7c353
Rob J Goedman <goedman@mac.com>
2019-07-08 14:14:42 -0600

    Revert back change to accepy CmdStanModels

--- src/stanmodel/number_of_chains.jl
+++ src/stanmodel/number_of_chains.jl
@@ -1,7 +1,7 @@
-function get_n_chains(model::T) where {T <: CmdStanModels}
+function get_n_chains(model::CmdStanModel)
   model.n_chains[1]
 end
 
-function set_n_chains(model::T, n_chains) where {T <: CmdStanModels}
+function set_n_chains(model::CmdStanModel, n_chains)
   model.n_chains[1] = n_chains
 end

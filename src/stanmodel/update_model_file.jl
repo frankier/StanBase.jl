0c811625e0c48fec46a1bdeec0687072a74b02d9
Rob J Goedman <goedman@mac.com>
2019-08-02 07:28:51 -0600

    Added #include handling in Stan language file

--- src/stanmodel/update_model_file.jl
+++ src/stanmodel/update_model_file.jl
@@ -24,6 +24,7 @@ function update_model_file(file::AbstractString, str::AbstractString)
   if isfile(file)
     resfile = open(file, "r")
     str2 = read(resfile, String)
+    str2 = parse_and_interpolate(str2)
     str != str2 && rm(file)
   end
   if str != str2
@@ -33,4 +34,28 @@ function update_model_file(file::AbstractString, str::AbstractString)
     close(strmout)
   end
   
+end
+
+function parse_and_interpolate(model::AbstractString)
+  #model = open(f->read(f, String), model)
+  newmodel = ""
+  lines = split(model, "\n")
+  for l in lines
+    ls = strip(l)
+    replace_strings = findall("#include", ls)
+    if length(replace_strings) == 1
+      for r in replace_strings
+        ls = split(strip(ls[r[end]+1:end]), " ")[1]
+        func = open(f -> read(f, String), strip(ls))
+        newmodel *= "   "*func*"\n"
+      end
+    else
+      if length(replace_strings) > 1
+        error("Inproper #includes")
+      else
+        newmodel *= l*"\n"
+      end
+    end
+  end
+  newmodel
 end

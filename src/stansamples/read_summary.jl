861e8e4d286c0390a27f7980407fe6c253e7a563
Rob J Goedman <goedman@mac.com>
2019-08-03 14:00:14 -0600

    Fixed read_summary based on cmdstan v2.20.0

--- src/stansamples/read_summary.jl
+++ src/stansamples/read_summary.jl
@@ -1,4 +1,4 @@
-using DataFrames, Unicode, DelimitedFiles, MCMCChains
+using DataFrames, MCMCChains, CSV
 
 """
 
@@ -8,69 +8,28 @@ Read summary output file created by stansummary.
 
 ### Method
 ```julia
-read_summary(model::CmdStanSampleModel)
+read_summary(m::Stanmodel)
 ```
 
 ### Required arguments
 ```julia
-* `model::CmdStanSampleModel`    : CmdStanSampleModel object
+* `m::Stanmodel`    : Stanmodel object
 ```
 
 """
-function read_summary(model::T) where {T <: CmdStanModels}
+function read_summary(m::Stanmodel)
+
+  fname = joinpath(m.tmpdir, "$(m.name)_summary.csv")
   
-  df = DataFrame()
+  df = CSV.read(fname, delim=",", comment="#")
   
-  file_path = "$(model.output_base)_summary.csv"
-  !isfile(file_path) && stan_summary(model)
-
-  if isfile(file_path)
-    instream = open(file_path)
-  else
-    println(pwd())
-    error("Summary file $(file_path) not found.")
-  end
-
-  skipchars(isspace, instream, linecomment='#')
-  #
-  # First non-comment line contains names of variables
-  #
-  line = Unicode.normalize(readline(instream), newline2lf=true)
-  idx = split(strip(line), ",")
-  index = [idx[k] for k in 1:length(idx)]
-  indvec = 1:length(index)
-
-  cnames = lowercase.(convert.(String, idx[indvec]))
+  cnames = lowercase.(convert.(String, String.(names(df))))
   cnames[1] = "parameters"
   cnames[4] = "std"
   cnames[8] = "ess"
-
-  rowno = 1; no_of_cols = 10
-  mat = [[]]  
-  for i in 1:no_of_cols-1
-    append!(mat, [[]])
-  end
-  row = Vector{Any}(undef, no_of_cols)
-  while !eof(instream)
-    skipchars(isspace, instream, linecomment='#')
-    line = Unicode.normalize(readline(instream), newline2lf=true)
-    if eof(instream) && length(line) < 2
-      close(instream)
-      break
-    else
-      skipchars(isspace, instream, linecomment='#')
-      line = split(line, ",")
-      append!(mat[1], [Symbol(line[1][2:end-1])])
-      for i in 2:no_of_cols
-        append!(mat[i], [parse.(Float64, line[i])])
-      end
-    end
-  end
-
-  for (i, var) in enumerate(cnames)
-    df[!, Symbol(var)] = mat[i]
-  end
+  names!(df, Symbol.(cnames))
+  df[!, :parameters] = Symbol.(df[!, :parameters])
   
   ChainDataFrame("CmdStan Summary", df)
   
-end   # end of read_summary
+end   # end of read_samples

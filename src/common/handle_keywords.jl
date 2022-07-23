5d714e5b86206e03fd23810b683f68b60a3c61ad
Rob J Goedman <goedman@icloud.com>
2022-07-23 16:57:17 -0600

    Rel 4.7.2 - Using @info instead of println()

--- src/common/handle_keywords.jl
+++ src/common/handle_keywords.jl
@@ -17,18 +17,17 @@ function handle_keywords!(m::T, kwrds) where {T <: CmdStanModels}
         :diagnostic_file, :cmdstan_home
     ]
     accepted_keywords = setdiff(model_keywords, excluded_model_keywords)
-    #println(accepted_keywords)
 
     for kwrd in keys(kwrds)
         if !(kwrd in [:data, :init])
             if kwrd in accepted_keywords
                 setfield!(m, kwrd, kwrds[kwrd])    
             else
-                println("\nAllowed keywords: $(accepted_keywords)")
+                @info "Allowed keywords: $(accepted_keywords)"
                 if kwrd in excluded_model_keywords
-                    println("\nKeyword \"$(Symbol(kwrd))\" not allowed. Ignored.")
+                    @warn "Keyword \"$(Symbol(kwrd))\" not allowed. Ignored."
                 else
-                    println("\nKeyword \"$(Symbol(kwrd))\" not in model. Ignored.")
+                    @warn "Keyword \"$(Symbol(kwrd))\" not in model. Ignored."
                 end
             end
         end
